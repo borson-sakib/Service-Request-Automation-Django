16a25a0279ff6a352ef6cf56ba8724a1ceae1819
borson-sakib <borson.sakib@northsouth.edu>
2022-12-28 12:26:11 +0600

    Initial Commit

---
+++ employeeInfo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmployeeinfoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'employeeInfo'
---
+++ employeeInfo/urls.py
@@ -0,0 +1,19 @@
+from telnetlib import LOGOUT
+from django.contrib import admin
+from django.urls import path
+from .views import *
+from django.contrib.auth import views as auth_views
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.middleware import AuthenticationMiddleware
+
+urlpatterns = [
+
+    path('', index, name="index"),
+    
+
+
+]
+
+
