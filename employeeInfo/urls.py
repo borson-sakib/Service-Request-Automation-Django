9b9ab587640f1ca6c72d352e078942c046930ac0
borson-sakib <borson.sakib@northsouth.edu>
2024-03-03 17:00:26 +0600

    UI Updates

--- employeeInfo/urls.py
+++ employeeInfo/urls.py
@@ -40,6 +40,6 @@ urlpatterns = [
     path('gini', gini, name="gini"),
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
